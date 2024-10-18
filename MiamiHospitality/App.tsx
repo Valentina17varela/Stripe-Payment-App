b5e23a13c88b6876855babb26db2aafae2a79c4a
Valentina17varela <valentina.varela17@outlook.com>
2024-10-18 07:13:28 -0500

    V1 - connect reader

--- MiamiHospitality/App.tsx
+++ MiamiHospitality/App.tsx
@@ -2,29 +2,64 @@ import 'react-native-gesture-handler';
 import React, { useEffect } from 'react';
 import AppNavigator from './navigation/AppNavigator';
 import { requestNeededAndroidPermissions, useStripeTerminal, StripeTerminalProvider } from '@stripe/stripe-terminal-react-native';
+import { Alert, PermissionsAndroid } from 'react-native';
 
 export default function App() {
   const { initialize } = useStripeTerminal();
 
-  const initializeStripeTerminal = async () => {
-    try {
-      const granted = await requestNeededAndroidPermissions({
-        accessFineLocation: {
-          title: 'Location Permission',
-          message: 'Stripe Terminal needs access to your location',
-          buttonPositive: 'Accept',
-        },
-      });
+  const requestBluetoothAndLocationPermissions = async () => {
+    if (true) {
+      try {
+        const granted = await requestNeededAndroidPermissions({
+          accessFineLocation: {
+            title: 'Location Permission',
+            message: 'Stripe Terminal needs access to your location',
+            buttonPositive: 'Accept',
+          },
+        });
+        if (granted) {
+          console.log('You can use the Location');
+        } else {
+          console.error(
+            'Location services are required in order to connect to a reader.'
+          );
+        }
 
-      if (granted) {     
-        initialize();
-      } else {
-        console.error(
-          'Location and Bluetooth services are required in order to connect to a reader.'
-        );
+        const grantedBluetooth = await PermissionsAndroid.request(
+          PermissionsAndroid.PERMISSIONS.BLUETOOTH_CONNECT,
+          {
+            title: 'Bluetooth Permission',
+            message: 'Stripe Terminal needs access to your Bluetooth',
+            buttonPositive: 'Accept',
+          }
+        )
+        if (grantedBluetooth) {
+          console.log('You can use the bluetooth');
+        } else {
+          console.error(
+            'Location services are required in order to connect to a reader.'
+          );
+        }
+
+        const grantedBluetoothScan = await PermissionsAndroid.request(
+          PermissionsAndroid.PERMISSIONS.BLUETOOTH_SCAN,
+          {
+            title: 'Bluetooth Permission',
+            message: 'Stripe Terminal needs access to your Bluetooth',
+            buttonPositive: 'Accept',
+          }
+        )
+        if (grantedBluetooth) {
+          console.log('You can use the bluetooth scan');
+        } else {
+          console.error(
+            'Location services are required in order to connect to a reader.'
+          );
+        }
+      }  catch (err) {
+        console.error('Error requesting permissions:', err);
+        return false;
       }
-    } catch (e) {
-      console.error('Error requesting permissions:', e);
     }
   };
 
@@ -40,8 +75,8 @@ export default function App() {
   };
 
   useEffect(() => {
-    initializeStripeTerminal();
-  }, [initialize]);
+    requestBluetoothAndLocationPermissions();
+  }, []);
 
   return (
     <StripeTerminalProvider logLevel='verbose' tokenProvider={fetchTokenProvider}>
--- MiamiHospitality/screens/PendingCheckIns.tsx
+++ MiamiHospitality/screens/PendingCheckIns.tsx
@@ -1,96 +1,188 @@
 import React, { useState, useEffect } from 'react';
-import { View, Text, FlatList, StyleSheet, ActivityIndicator, Alert, TouchableOpacity, SafeAreaView } from 'react-native';
+import { View, Text, FlatList, StyleSheet, ActivityIndicator, Alert, TouchableOpacity, SafeAreaView, Modal } from 'react-native';
 import axios from 'axios';
+import { useStripeTerminal } from '@stripe/stripe-terminal-react-native';
 
 const PendingCheckIns = () => {
-  const [pendingCheckIns, setPendingCheckIns] = useState([]);
-  const [loading, setLoading] = useState(true);
-  const [error, setError] = useState('');
-  const [readersList, setReadersList] = useState<{ id: string; label: string; device_type: string }[]>([]);
-  const [isModalVisible, setModalVisible] = useState(false);
-  const [selectedReader, setSelectedReader] = useState(null);
+    const { initialize } = useStripeTerminal();
+    const [pendingCheckIns, setPendingCheckIns] = useState([]);
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState('');
+    const [isReaderConnected, setIsReaderConnected] = useState(false);
+    const [selectedReader, setSelectedReader] = useState<any>(null);
+    const [isDiscovering, setIsDiscovering] = useState(false); // Controla si está buscando lectores
+    const [isModalVisible, setModalVisible] = useState(false); // Controla la visibilidad del modal
+    const [discoveredReadersState, setDiscoveredReadersState] = useState([]);
 
-  useEffect(() => {
-    fetchPendingCheckIns();
-    fetchStripeReader();
+    // Stripe Terminal hooks
+    const { discoverReaders, connectBluetoothReader, discoveredReaders, createPaymentIntent, collectPaymentMethod } = useStripeTerminal();
 
-    const interval = setInterval(() => {
-      fetchPendingCheckIns();
-    }, 180000);
+    useEffect(() => {
+        initialize();
+    }, [initialize]);
 
-    return () => clearInterval(interval);
-  }, []);
+    useEffect(() => {
+        fetchPendingCheckIns();
+        const interval = setInterval(() => {
+            fetchPendingCheckIns();
+        }, 180000);
+        return () => clearInterval(interval);
+    }, []);
 
-  const fetchPendingCheckIns = async () => {
-    try {
-      const response = await axios.get('https://email.mvr-management.com/arya_pending_checkins');
-      setPendingCheckIns(response.data.data);
-      setLoading(false);
-    } catch (err) {
-      setError('Error fetching data');
-      setLoading(false);
-      console.log('Error fetching data:', error);
-    }
-  };
+    const fetchPendingCheckIns = async () => {
+        try {
+            const response = await axios.get('https://email.mvr-management.com/arya_pending_checkins');
+            setPendingCheckIns(response.data.data);
+            setLoading(false);
+        } catch (err) {
+            setError('Error fetching data');
+            setLoading(false);
+            console.log('Error fetching data:', error);
+        }
+    };
 
-  const fetchStripeReader = async () => {
-    try {
-      const response = await axios.get('https://email.mvr-management.com/stripe_readers');
-      setReadersList(response.data.readers);
-    } catch (err) {
-      console.log('Error fetching data stripe:', error);
-    }
-  };
+    const handleDiscoverReaders = async () => {
+        if (isDiscovering) {
+            console.log('Reader discovery already in progress');
+            return;
+        }
 
-  const handlePayment = async (checkIn: { customer: any; property: any; charge: any }) => {
-    Alert.alert('Payment successful');
-  };
+        setIsDiscovering(true);
 
-  const renderItem = ({ item }: { item: { customer: string; property: string; checkin: string; checkout: string; charge: number; external_id: string } }) => (
-    <View style={styles.item}>
-      <Text style={styles.title}>{item.customer}</Text>
-      <Text style={styles.subtitle}>{item.property}</Text>
-      <Text style={styles.text}>{item.checkin} - {item.checkout}</Text>
-      
-      <View style={styles.footerContainer}>
-        <Text style={styles.charge}>${item.charge}</Text>
-        <TouchableOpacity 
-          onPress={() => handlePayment(item)}
-          style={styles.button}
-        >
-          <Text style={styles.buttonText}>Process Payment</Text>
-        </TouchableOpacity>
-      </View>
-    </View>
-  );
+        const { error } = await discoverReaders({
+            discoveryMethod: 'bluetoothScan',
+            simulated: true,
+        });
 
-  return (
-    <SafeAreaView style={styles.container}>
-      <View style={styles.header}>
-        <Text style={styles.headerTitle}>Hotel Arya</Text>
-        <TouchableOpacity onPress={() => setModalVisible(true)}>
-            <Text style={styles.headerIcon}>Connect Reader</Text>
-        </TouchableOpacity>
-      </View>
-      <Text style={styles.subHeader}>Pending Check-Ins</Text>
-      {loading ? (
-        <View style={styles.centered}>
-          <ActivityIndicator size="large" color="#3b82f6" />
+        setIsDiscovering(false);
+
+        if (error) {
+            Alert.alert('Discover readers error', `${error.code}: ${error.message}`);
+        } else if (discoveredReaders.length > 0) {
+            handleConnectBluetoothReader(discoveredReaders[0]);
+        } else {
+            Alert.alert('No readers found', 'Please try again.');
+        }
+    };
+
+    const handleConnectBluetoothReader = async (reader: any) => {
+        const { reader: connectedReader, error } = await connectBluetoothReader({
+            reader,
+            locationId: reader.locationId, // Asegúrate de que el lector tenga una ubicación asignada
+        });
+
+        if (error) {
+            Alert.alert('Connection Error', `${error.code}: ${error.message}`);
+        } else {
+            setSelectedReader(connectedReader);
+            setIsReaderConnected(true);
+            Alert.alert('Reader Connected', `Connected to: ${connectedReader.label}`);
+        }
+    };
+
+    const handlePayment = async (checkIn: { customer?: string; property?: string; checkin?: string; checkout?: string; charge: any; external_id?: string; }) => {
+        if (!isReaderConnected) {
+            Alert.alert('No Reader', 'Please connect a reader first.');
+            return;
+        }
+
+        try {
+            // Crea un PaymentIntent
+            const { paymentIntent, error: createError } = await createPaymentIntent({
+                amount: checkIn.charge * 100, // Multiplica por 100 porque Stripe trabaja con centavos
+                currency: 'usd',
+            });
+
+            if (createError) {
+                Alert.alert('Payment Error', `Failed to create payment intent: ${createError.message}`);
+                return;
+            }
+
+            // Recoge el método de pago utilizando el lector
+            // const { error: collectError } = await collectPaymentMethod(paymentIntent.id);
+            // if (collectError) {
+            //     Alert.alert('Payment Error', `Failed to collect payment: ${collectError.message}`);
+            //     return;
+            // }
+        } catch (err) {
+            Alert.alert('Payment Error', `Error processing payment`);
+        }
+    };
+
+    const renderItem = ({ item }: { item: { customer: string; property: string; checkin: string; checkout: string; charge: number; external_id: string; } }) => (
+        <View style={styles.item}>
+            <Text style={styles.title}>{item.customer}</Text>
+            <Text style={styles.subtitle}>{item.property}</Text>
+            <Text style={styles.text}>{item.checkin} - {item.checkout}</Text>
+            
+            <View style={styles.footerContainer}>
+                <Text style={styles.charge}>${item.charge}</Text>
+                <TouchableOpacity 
+                    onPress={() => handlePayment(item)}
+                    style={styles.button}
+                >
+                    <Text style={styles.buttonText}>Process Payment</Text>
+                </TouchableOpacity>
+            </View>
         </View>
-      ) : error ? (
-        <View style={styles.centered}>
-          <Text>{error}</Text>
+    );
+
+    return (
+        <SafeAreaView style={styles.container}>
+            <View style={styles.header}>
+                <Text style={styles.headerTitle}>Hotel Arya</Text>
+                <TouchableOpacity onPress={handleDiscoverReaders}>
+                    <Text style={styles.headerIcon}>Connect Reader</Text>
+                </TouchableOpacity>
+            </View>
+            <Text style={styles.subHeader}>Pending Check-Ins</Text>
+            {loading ? (
+                <View style={styles.centered}>
+                    <ActivityIndicator size="large" color="#3b82f6" />
+                </View>
+            ) : error ? (
+                <View style={styles.centered}>
+                    <Text>{error}</Text>
+                </View>
+            ) : (
+                <FlatList
+                    data={pendingCheckIns}
+                    renderItem={renderItem}
+                    keyExtractor={(item) => item.external_id}
+                    contentContainerStyle={styles.listContent}
+                />
+            )}
+
+<Modal
+        visible={isModalVisible}
+        transparent={true}
+        animationType="slide"
+        onRequestClose={() => setModalVisible(false)}
+      >
+        <View style={styles.modalContainer}>
+          <View style={styles.modalContent}>
+            <Text style={styles.modalTitle}>Select a Reader</Text>
+            {discoveredReaders.length > 0 ? (
+              <FlatList
+                data={discoveredReaders}
+                keyExtractor={(reader) => reader.serialNumber}
+                renderItem={({ item: reader }) => (
+                  <TouchableOpacity onPress={() => handleConnectBluetoothReader(reader)}>
+                    <Text style={styles.readerItem}>{reader.deviceType} - {reader.serialNumber}</Text>
+                  </TouchableOpacity>
+                )}
+              />
+            ) : (
+              <Text>No readers found</Text>
+            )}
+            <TouchableOpacity style={styles.closeButton} onPress={() => setModalVisible(false)}>
+              <Text style={styles.closeButtonText}>Close</Text>
+            </TouchableOpacity>
+          </View>
         </View>
-      ) : (
-        <FlatList
-          data={pendingCheckIns}
-          renderItem={renderItem}
-          keyExtractor={(item) => item.external_id}
-          contentContainerStyle={styles.listContent}
-        />
-      )}
-    </SafeAreaView>
-  );
+      </Modal>
+        </SafeAreaView>
+    );
 };
 
 const styles = StyleSheet.create({
@@ -105,6 +197,8 @@ const styles = StyleSheet.create({
     padding: 16,
   },
   headerIcon: {
+    color: 'white',
+    fontSize: 16,
     marginRight: 8,
   },
   headerTitle: {
@@ -181,6 +275,38 @@ const styles = StyleSheet.create({
     fontSize: 16,
     fontWeight: 'bold',
     marginLeft: 8,
+  },
+  modalContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'rgba(0, 0, 0, 0.5)',
+  },
+  modalContent: {
+    backgroundColor: 'white',
+    padding: 20,
+    borderRadius: 10,
+    width: '80%',
+    alignItems: 'center',
+  },
+  modalTitle: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    marginBottom: 20,
+  },
+  readerItem: {
+    paddingVertical: 10,
+    fontSize: 16,
+  },
+  closeButton: {
+    backgroundColor: '#3b82f6',
+    padding: 10,
+    borderRadius: 5,
+    marginTop: 20,
+  },
+  closeButtonText: {
+    color: 'white',
+    fontWeight: 'bold',
   },
 });
 
